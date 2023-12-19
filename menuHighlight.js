b5ae731cea3317cba08f5654b01a6b390459ef83
DekiruDeku <89838558+DekiruDeku@users.noreply.github.com>
2023-12-19 11:54:29 +0300

    UPDATE MENUHIGHLIGHT

--- menuHighlight.js
+++ menuHighlight.js
@@ -1,49 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Определение текущего пути
-  var currentPath = window.location.pathname;
+  var currentPath = window.location.pathname.split('/').pop();
 
   // Определение активного пункта меню
-  var activeMenuItem1;
-  var activeMenuItem2;
-
-  if (currentPath.includes("/index.html")) {
-    activeMenuItem1 = document.querySelector("nav ul li a[href='index.html']");
-  } else if (currentPath.includes("/tablepage.html")) {
-    activeMenuItem1 = document.querySelector(
-      "nav ul li a[href='tablepage.html']"
-    );
-  } else if (currentPath.includes("/database.html")) {
-    activeMenuItem1 = document.querySelector(
-      "nav ul li a[href='database.html']"
-    );
-  } else if (currentPath.includes("/guestmembers.html")) {
-    activeMenuItem1 = document.querySelector(
-      "nav ul li a[href='guestmembers.html']"
-    );
-  }
-  if (currentPath.includes("/index.html")) {
-    activeMenuItem2 = document.querySelector(
-      "div nav ul li a[href='index.html']"
-    );
-  } else if (currentPath.includes("/tablepage.html")) {
-    activeMenuItem2 = document.querySelector(
-      "div nav ul li a[href='tablepage.html']"
-    );
-  } else if (currentPath.includes("/database.html")) {
-    activeMenuItem2 = document.querySelector(
-      "div nav ul li a[href='database.html']"
-    );
-  } else if (currentPath.includes("/guestmembers.html")) {
-    activeMenuItem2 = document.querySelector(
-      "div nav ul li a[href='guestmembers.html']"
-    );
-  }
+  var activeMenuItem1 = document.querySelector(`nav ul li a[href='${currentPath}']`);
+  var activeMenuItem2 = document.querySelector(`div nav ul li a[href='${currentPath}']`);
 
   // Добавление класса "active" к активному пункту меню
-  if (activeMenuItem1) {
-    activeMenuItem1.classList.add("active");
-  }
-  if (activeMenuItem2) {
-    activeMenuItem2.classList.add("active");
-  }
+  if (activeMenuItem1) activeMenuItem1.classList.add("active");
+  if (activeMenuItem2) activeMenuItem2.classList.add("active");
 });
